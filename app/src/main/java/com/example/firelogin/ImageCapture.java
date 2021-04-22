6d9a4f4012d2ea50c716a69771acbf11fba6c7e4
Rkaur <rupinder88uiet@gmail.com>
2021-04-22 00:30:17 -0400

    ImageCapture

--- app/src/main/java/com/example/firelogin/ImageCapture.java
+++ app/src/main/java/com/example/firelogin/ImageCapture.java
@@ -11,6 +11,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -18,9 +20,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+
 public class ImageCapture extends AppCompatActivity {
     public static final int CAMERA_PERMISSION = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int REQUEST_CODE_STORAGE_PERMISSION = 103;
+    public static final int REQUEST_CODE_SELECT_IMAGE = 104;
 
     ImageView selectedimage;
     Button camerabtn,gallerybtn;
@@ -44,9 +50,25 @@ public class ImageCapture extends AppCompatActivity {
         gallerybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ImageCapture.this, "gallery button is clicked", Toast.LENGTH_LONG).show();
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ImageCapture.this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE_PERMISSION);
+                }
+                else{
+                    selectImage();
+                }
+
+                //Toast.makeText(ImageCapture.this, "gallery button is clicked", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void selectImage() {
+        Intent intent= new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
+        }
+
+
     }
 
     private void askCameraPermissions() {
@@ -66,14 +88,24 @@ public class ImageCapture extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
         if(requestCode==CAMERA_PERMISSION){
             if(grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 openCamera();
             }
             else{
-                Toast.makeText(this, "Camera Permission is required to use Camera", Toast.LENGTH_LONG).show();
+                 Toast.makeText(this, "Camera Permission is required to use Camera", Toast.LENGTH_LONG).show();
             }
         }
+        else if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length>0){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                selectImage();
+            }
+            else{
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+        else{}
     }
 
     @Override
@@ -82,6 +114,21 @@ public class ImageCapture extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST_CODE) {
             Bitmap image = (Bitmap) data.getExtras().get("data");
             selectedimage.setImageBitmap(image);
+        }
+        else if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode ==RESULT_OK){
+            if(data !=null){
+                Uri selectedImageUri =data.getData();
+                if(selectedImageUri != null){
+                    try{
+                        InputStream inputStream =getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
+                        selectedimage.setImageBitmap(bitmap);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
         }
     }
 }
